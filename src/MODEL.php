19ba1369cbf2688c3eb950662f5d9e1e1e1aa71a
Desmond Chin <desmond.chin.cc93@gmail.com>
2019-10-29 01:02:05 +0800

    updates

--- src/MODEL.php
+++ src/MODEL.php
@@ -3,7 +3,26 @@
 namespace MightyORM;
 
 class MODEL {
+
+    protected $table = '';
+    protected $connection = '';
+
     public function __construct() {
-        
+
+        /**
+         * If table not declared, assume the model class name
+         */
+        if(empty($this->table)){
+            $this->table = str_replace('Model', '', static::class);
+        }
+
+        /**
+         * If connection not declared, assume the default
+         */
+        if(empty($this->connection)){
+            $this->connection = 'default';
+        }
+
+        DB\Connect($this->connection);
     }
 }
